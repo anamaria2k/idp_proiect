2f1ce3e4f04858c724be48d11825a95cec457294
Ana Tuca <anamaria.tucaa@gmail.com>
2020-03-04 22:41:24 +0200

    Changed the graphic interface

--- userclient/src/userclient.py
+++ userclient/src/userclient.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 
 @app.route("/home", methods=["GET","POST"])
 def home_page():
-	return render_template("home.html")
+	return render_template("login.html")
 
 if __name__ == '__main__':
 	app.run(host='0.0.0.0',port='4000',debug=True)
